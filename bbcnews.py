e4c95b56d1113a6a1899e0587d9dbd4215c99e91
Stez <zetsrm@protonmail.com>
2020-07-04 13:57:40 +0100

    bbc news scraper
    will scrape top 10 headlines and corresponding urls from bbc news frontpage

---
+++ bbcnews.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+from bs4 import BeautifulSoup
+from termcolor import colored, cprint
+
+# In[4]:
+
+
+baseurl = "https://www.bbc.co.uk"
+scrape_url = "https://www.bbc.co.uk/news"
+
+
+# In[5]:
+
+
+response = requests.get(scrape_url)
+
+
+# In[6]:
+
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+# In[29]:
+
+
+headlineouter = soup.find('a', class_ = "gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-paragon-bold nw-o-link-split__anchor")
+headlineinner = headlineouter.find('h3')
+
+
+# In[32]:
+
+
+#print the main headline and accompanying url
+cprint(f"{headlineinner.text}", 'red', attrs=['bold'], end=' ')
+cprint(f"({baseurl}{headlineouter['href']})", 'blue', end='\n')
+
+
+# In[71]:
+
+
+articles = soup.find_all('a',class_="gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor")
+
+
+# In[74]:
+
+
+i = 0
+links_to_fetch = 10
+while i < links_to_fetch:
+    articleurl = articles[i].get('href')
+    articleheadline = articles[i].h3.text
+    cprint(f"{articleheadline}:", 'red', attrs=['bold'],end='')
+    cprint(f"({baseurl}{articleurl})", 'blue', end='\n')
+    i+=1
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
