4208d13c9420a13bf02de19b6930fafdf9bfde35
Stez <zetsrm@protonmail.com>
2020-07-07 13:21:22 +0100

    removed global variables, replaced while loops and single char variables

--- bbc.py
+++ bbc.py
@@ -10,11 +10,13 @@ import sys
 #vars
 baseurl = "https://www.bbc.co.uk"
 scrape_url = "https://www.bbc.co.uk/news"
-x = 1
 stories = ['null']
 
-def get_headlines():
-	global x
+#def get_soup(url):
+	
+
+def get_headlines(item_start_id):
+	current_item = item_start_id
 	#request and parse html
 	response = requests.get(scrape_url)
 	soup = BeautifulSoup(response.text, "html.parser")
@@ -25,22 +27,22 @@ def get_headlines():
 	headline_url = headlineouter['href']
 	stories.append(headline_url)
 	#print the main headline and accompanying url
-	cprint(f"[{x}]{headlineinner.text}", 'red', attrs=['bold'], end='\n')
-	x+=1
+	cprint(f"[{current_item}]{headlineinner.text}", 'red', attrs=['bold'], end='\n')
+	current_item+=1
 	
 	#isolate the rest of the headlines (articles)
 	articles = soup.find_all('a',class_="gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor")
 
 	#extract headline and url from links on main page & print
-	i = 0
+
 	links_to_fetch = 10
-	while i < links_to_fetch:
-		articleurl = articles[i].get('href')
-		articleheadline = articles[i].h3.text
+	for item in range(links_to_fetch):
+		articleurl = articles[item].get('href')
+		articleheadline = articles[item].h3.text
 		stories.append(articleurl)
-		cprint(f"[{x}]{articleheadline}:", 'red', attrs=['bold'],end='\n')
-		x+=1
-		i+=1
+		cprint(f"[{current_item}]{articleheadline}:", 'red', attrs=['bold'],end='\n')
+		current_item+=1
+
 
 def get_article_text(partial_url):
 	global baseurl
@@ -48,22 +50,22 @@ def get_article_text(partial_url):
 	response = requests.get(complete_url)
 	soup = BeautifulSoup(response.text, "html.parser")
 	story = soup.find_all('p')
-	#i is set to 12 to ignore all the pre-amble / social media ads.
+	#range_start is set to 12 to ignore all the pre-amble / social media ads.
 	#range is -4 to remove unnecessary text
-	i = 12
-	while i < (len(story)-4):
-		print(story[i].text)
-		i+=1
+	range_start = 12
+	for item in range(len(story)-4):
+		if item < range_start:
+			pass
+		else:
+			print(story[item].text)
+
 	cprint("Press any key to return to Main Menu", 'red', attrs=['bold'], end='\n')
 	input()
 
 def main():
-	global x
-	x = 1 
-	get_headlines()
+	get_headlines(1)
 	article_id = input("select an article id or Q to quit\n")
-
-	if article_id == 'Q' or article_id == 'q':
+	if article_id.lower() == 'q':
 		cprint("Goodbye!", 'red', end='\n')
 		sys.exit()
 	
